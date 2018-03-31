9eb6e6e8c6f72b4320ba0a8561f168d0c64eb266
lukeclement <lclement.57468@farnborough.ac.uk>
2018-03-31 23:22:16 +0100

    Working on it still

--- TheLatest/Mandel.java
+++ TheLatest/Mandel.java
@@ -1,3 +1,63 @@
-public class Mandel{
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.canvas.*;
+import javafx.animation.*;
+import javafx.scene.image.*;
+import java.util.*;
+import java.awt.image.*;
+import javafx.scene.input.*;
+import javafx.event.*;
+import javafx.scene.paint.*;
+import java.io.*;
+import javax.imageio.*;
+import javafx.embed.swing.*;
 
+public class Mandel extends Application{
+    public int width=1000;
+    public int height=1000;
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    //Where the magic happens
+    public void start(Stage mainStage){
+        Group root=new Group();
+        Scene scene=new Scene(root);
+        mainStage.setScene(scene);
+        
+        Canvas canvas=new Canvas(width,height);
+        root.getChildren().add(canvas);
+        GraphicsContext gc=canvas.getGraphicsContext2D();
+        
+        //For key presses
+        List<String> input=new ArrayList<>();
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+            public void handle(KeyEvent e){
+                String code = e.getCode().toString();
+                // only add once... prevent duplicates
+                if ( !input.contains(code) )
+                    input.add( code );
+            }
+        });
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>(){
+            public void handle(KeyEvent e){
+                String code = e.getCode().toString();
+                input.remove( code );
+            }
+        });
+        
+        //Starting animation
+        final long startNanoTime=System.nanoTime();
+        
+        new AnimationTimer(){
+            public void handle(long currentNanoTime){
+                //Stuff here
+                
+                //End stuff
+            }
+        }.start();
+        mainStage.show();
+    }
 }
--- TheLatest/Part.java
+++ TheLatest/Part.java
@@ -1,3 +1,25 @@
 public class Part{
-
+    private double posX;
+    private double posY;
+    private int x;
+    private int y;
+    
+    public Part(int x, int y, double scale, double offX, double offY){
+        this.x=x;
+        this.y=y;
+        posX=(double)x*scale-offX;
+        posY=offY-(double)y*scale;
+    }
+    
+    public getValue(){
+        Complex c=new Complex(posX,posY);
+        Complex z=new Complex(0,0);
+        int iterations=0
+        while((z.getReal()<=2.0E+307||z.getImaginary()<=2.0E+307)&&loops<255){
+            z=(z.square()).plus(c);
+            iterations++;
+        }
+        return iterations;
+    }
+    
 }
